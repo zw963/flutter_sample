4c139d7facc18e8163981dee1a51546e8a2c19f2
Eric Dallo <ericdallo06@hotmail.com>
2020-03-04 00:57:05 -0300

    Add animation sample

--- lib/screens/dashboard/components/rightscreen.dart
+++ lib/screens/dashboard/components/rightscreen.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sample/screens/dashboard/screens/animation.dart';
 
 class RightScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildButton(context, color: Colors.yellow),
+          Expanded(flex: 1, child: Center()),
+          buildButton(context, color: Colors.blue),
+        ],
+      ),
+    );
+  }
+
+  Widget buildButton(context, {Color color}) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RaisedButton(
+            color: color,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10.0))),
+            elevation: 5.0,
+            child: Container(
+              height: 70,
+              child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.code, color: Colors.white)),
+            ),
+            onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => AnimationScreen())),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/screens/dashboard/screens/animation.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class AnimationScreen extends StatefulWidget {
+  @override
+  _AnimationScreenState createState() => _AnimationScreenState();
+}
+
+class _AnimationScreenState extends State<AnimationScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<Offset> leftAnimation;
+  Animation<Offset> rightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 2000),
+    );
+    
+    leftAnimation = Tween<Offset>(
+      begin: Offset(-1.5, 0),
+      end: const Offset(1.5, 0.0),
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.decelerate,
+    ));
+
+    rightAnimation = Tween<Offset>(
+      begin: Offset(4.5, 0),
+      end: const Offset(1.5, 0.0),
+    ).animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.decelerate,
+    ));
+    
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SlideTransition(
+            child: Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 100,
+            ),
+            position: leftAnimation,
+          ),
+          SlideTransition(
+            child: Icon(
+              Icons.map,
+              color: Colors.blue,
+              size: 100,
+            ),
+            position: rightAnimation,
+          ),
+          SlideTransition(
+            child: Icon(
+              Icons.sms,
+              color: Colors.purple,
+              size: 100,
+            ),
+            position: leftAnimation,
+          ),
+        ],
+      ),
+    );
+  }
+}
