05fea7453d950e6acd9083afe43936c0c05953f6
Eric Dallo <ericdallo06@hotmail.com>
2021-04-10 17:03:32 -0300

    Fix missing wrong package

--- lib/screens/dashboard/components/leftscreen.dart
+++ lib/screens/dashboard/components/leftscreen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:linear_progress_loading/linear_progress_loading.dart';
+// import 'package:linear_progress_loading/linear_progress_loading.dart';
 
 class LeftScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        LinearProgressLoading.trickling(
-          foregroundColor: Colors.blueAccent,
-        ),
+        // LinearProgressLoading.trickling(
+        //   foregroundColor: Colors.blueAccent,
+        // ),
       ],
     );
   }
