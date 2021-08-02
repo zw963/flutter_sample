cb6e7e79b91d994fd90ca0e373b76c0062e9aee9
Eric Dallo <ericdallo06@hotmail.com>
2021-08-02 11:56:39 -0300

    Fix local package

--- lib/screens/dashboard/components/leftscreen.dart
+++ lib/screens/dashboard/components/leftscreen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:linear_progress_loading/linear_progress_loading.dart';
+// import 'package:linear_progress_loading/linear_progress_loading.dart';
 
 class LeftScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        LinearProgressLoading.trickling(
-          foregroundColor: Colors.blueAccent,
-        ),
+        Container(),
+        // LinearProgressLoading.trickling(
+        //   foregroundColor: Colors.blueAccent,
+        // ),
       ],
     );
   }
