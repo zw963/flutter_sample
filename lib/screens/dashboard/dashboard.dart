3e482066c7c69e063adbb57b91da1ef10a2e35ae
Billy.Zheng <vil963@gmail.com>
2022-03-16 01:45:26 +0800

    Update

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +10,24 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body(),
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            final window = await DesktopMultiWindow.createWindow(jsonEncode({
+              'args1': 'Sub window',
+              'args2': 100,
+              'args3': true,
+              'bussiness': 'bussiness_test',
+            }));
+            window
+              ..setFrame(const Offset(0, 0) & const Size(1280, 720))
+              ..center()
+              ..setTitle('Another window')
+              ..show();
+          },
+          child: Text('click me'),
+        ),
+      ),
     );
   }
 }
